efa82621b65580fad68393a845e7d3908f596896
doadin <tbkizle@gmail.com>
2021-12-23 19:28:19 -0500

    Update All.ps1

--- PS/All.ps1
+++ PS/All.ps1
@@ -118,7 +118,7 @@ function Build64Deluge {
     if ( -not (Test-Path 'C:\nasm-2.15' -PathType Container) ) { 
         Write-Host "Installing NASM..."
         Set-Location -Path 'C:\DelugeDownloads\'
-        7z x nasm-2.14.02.zip -oc:\
+        7z x nasm-2.15-win64.zip -oc:\
     }
     ## if ( -not (Test-Path 'C:\Perl64' -PathType Container) ) { 
     ##     Write-Host "Installing Active State Perl..."
