2d8197fc7cb7444f1b0eb23e5456c70c32c04609
doadin <tbkizle@gmail.com>
2022-01-20 08:47:17 -0500

    Update All.ps1

--- PS/All.ps1
+++ PS/All.ps1
@@ -185,7 +185,6 @@ function Build64Deluge {
     }
 
     $env:Path = "C:\Program Files\Git\bin;C:\Program Files\7-Zip;C:\Python37;C:\Python37\Scripts;C:\boost_1_77_0;C:\nasm-2.15.05;C:\Perl64\perl\bin;C:\msys64\usr\bin;$env:Path"
-    $env:Path += ";C:\Program Files\Git\bin;C:\Program Files\7-Zip;C:\Python37;C:\Python37\Scripts;C:\boost_1_77_0;C:\nasm-2.15.05;C:\Perl64\perl\bin;C:\msys64\usr\bin;"
     $env:BOOST_ROOT="c:\boost_1_77_0"
     $env:BOOST_BUILD_PATH="c:\boost_1_77_0\tools\build"
     Invoke-BatchFile "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Community\VC\Auxiliary\build\vcvars64.bat"
@@ -224,7 +223,6 @@ function Build64Deluge {
     ## python build.py -d build --clean --gtk3-ver=3.24 --vs-ver=15 --platform=x64 --same-python -k --enable-gi --py-wheel --py-egg enchant gtk gtk3-full pycairo pygobject lz4
     
     $env:Path = "C:\nsis-3.08;C:\gtk-build\gtk\x64\release\bin;C:\Program Files\Git\bin;C:\Program Files\7-Zip;C:\Python37;C:\Python37\Scripts;C:\boost_1_77_0;C:\nasm-2.15.05;C:\Perl64\perl\bin;C:\msys64\usr\bin;$env:Path"
-    $env:Path += ";C:\nsis-3.08;C:\gtk-build\gtk\x64\release\bin;C:\Program Files\Git\bin;C:\Program Files\7-Zip;C:\Python37;C:\Python37\Scripts;C:\boost_1_77_0;C:\nasm-2.15.05;C:\Perl64\perl\bin;C:\msys64\usr\bin;"
     
     $libtorrentpath = Get-Childitem –Path "C:\libtorrent-RC_1_2\bindings\python" -Include libtorrent.pyd -File -Recurse -ErrorAction SilentlyContinue | select -expand FullName
     Move-Item –Path $libtorrentpath -Destination "C:\Python37\lib\site-packages\libtorrent.pyd"
@@ -258,158 +256,6 @@ function Build64Deluge {
     #makensis deluge-win32-installer.nsi
     
     
-}
-
-function Build32Deluge {
-    $WebClient = New-Object System.Net.WebClient
-    
-    if ( -not (Test-Path 'C:\DelugeDownloads\vs_Community.exe' -PathType Leaf) ) { 
-        Write-Host "Downloading Visual Studio 2017 Community..."
-        $WebClient.DownloadFile("https://download.visualstudio.microsoft.com/download/pr/57c99182-2828-4fc1-8b36-271e2472c838/91d7b8d8ba556b1e8723ca67a0f1e2e4a8274f7dfccbeae567e78b22f0db3f6d/vs_Community.exe","C:\DelugeDownloads\vs_Community.exe")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\python-3.7.9.exe' -PathType Leaf) ) { 
-        Write-Host "Downloading Python..."
-        $WebClient.DownloadFile("https://www.python.org/ftp/python/3.7.9/python-3.7.9.exe","C:\DelugeDownloads\python-3.7.9.exe")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\nasm-2.15-win32.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading Nasm..."
-        $WebClient.DownloadFile("https://www.nasm.us/pub/nasm/releasebuilds/2.15/win32/nasm-2.15-win32.zip","C:\DelugeDownloads\nasm-2.15-win32.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\strawberry-perl-5.32.1.1-32bit-portable.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading ActivePerl..."
-        $WebClient.DownloadFile("https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-32bit-portable.zip","C:\DelugeDownloads\strawberry-perl-5.32.1.1-32bit-portable.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\msys2-i686-20200517.exe' -PathType Leaf) ) { 
-        Write-Host "Downloading Msys2..."
-        $WebClient.DownloadFile("http://repo.msys2.org/distrib/i686/msys2-i686-20200517.exe","C:\DelugeDownloads\msys2-i686-20200517.exe")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\boost_1_77_0.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading Boost C++ Source Code..."
-        $WebClient.DownloadFile("https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.zip","C:\DelugeDownloads\boost_1_77_0.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\openssl.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading OpenSSL Source Code..."
-        $WebClient.DownloadFile("https://github.com/openssl/openssl/archive/master.zip","C:\DelugeDownloads\openssl.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\gvsbuild.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading GTK3+ gvsbuild tools..."
-        $WebClient.DownloadFile("https://github.com/wingtk/gvsbuild/archive/master.zip","C:\DelugeDownloads\gvsbuild.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\libtorrent.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading Libtorrent Source Code..."
-        $WebClient.DownloadFile("https://github.com/arvidn/libtorrent/archive/RC_1_2.zip","C:\DelugeDownloads\libtorrent.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\Deluge.zip' -PathType Leaf) ) { 
-        Write-Host "Downloading Deluge Source Code..."
-        $WebClient.DownloadFile("https://github.com/deluge-torrent/deluge/archive/develop.zip","C:\DelugeDownloads\deluge.zip")
-    }
-    if ( -not (Test-Path 'C:\DelugeDownloads\7z1900-x64.msi' -PathType Leaf) ) { 
-        Write-Host "Downloading 7-zip..."
-        $WebClient.DownloadFile("https://www.7-zip.org/a/7z1900-x64.msi","C:\DelugeDownloads\7z1900-x64.msi")
-    }
-    
-    if ( -not (Test-Path 'C:\Program Files\7-Zip' -PathType Container) ) { 
-        Write-Host "Installing 7-Zip..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        Start-Process -FilePath "C:\DelugeDownloads\7z1900-x64.msi" -ArgumentList "/quiet", "/passive", "/norestart" -Wait
-    }
-    if ( -not (Test-Path 'C:\Program Files (x86)\Microsoft Visual Studio\2017' -PathType Container) ) { 
-        Write-Host "Installing Visual Studio 2017 Community..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        New-Item -Path "C:\DelugeDownloads\vsconfig.conf"
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '    {'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '        "version": "1.0",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '        "components": ['
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.CoreEditor",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Workload.CoreEditor",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.Roslyn.Compiler",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.Component.MSBuild",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.Static.Analysis.Tools",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.TextTemplating",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.Debugger.JustInTime",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.ComponentGroup.WebToolsExtensions",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.VC.CoreIde",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.VC.Redist.14.Latest",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.Graphics.Win81",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.Graphics.Tools",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.VC.DiagnosticTools",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.Windows10SDK.17763",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.VC.CMake.Project",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Component.VC.ATL",'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '          "Microsoft.VisualStudio.Workload.NativeDesktop"'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '        ]'
-        Add-Content -Path "C:\DelugeDownloads\vsconfig.conf" -Value '      }'
-        Start-Process -FilePath "C:\DelugeDownloads\vs_Community.exe" -ArgumentList "--norestart", "--quiet", "--config C:\DelugeDownloads\vsconfig.conf" -Wait
-    }
-    if ( -not (Test-Path 'C:\Program Files\Python37' -PathType Container) ) { 
-        Write-Host "Installing Python..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        Start-Process -FilePath "C:\DelugeDownloads\python-3.7.6.exe" -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=0", "Include_test=0" -Wait
-    }
-    if ( -not (Test-Path 'C:\nasm-2.15' -PathType Container) ) { 
-        Write-Host "Installing NASM..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        7z x nasm-2.14.02.zip -oc:\
-    }
-    ## if ( -not (Test-Path 'C:\Perl64' -PathType Container) ) { 
-    ##     Write-Host "Installing Active State Perl..."
-    ##     Set-Location -Path 'C:\DelugeDownloads\'
-    ##     Start-Process -FilePath "C:\DelugeDownloads\strawberry-perl-5.32.1.1-64bit.msi" -ArgumentList "/exenoui", "/exenoupdates" -Wait
-    ## }
-    if ( -not (Test-Path 'C:\Perl' -PathType Container) ) { 
-        Write-Host "Installing Strawberry Perl..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        7z x strawberry-perl-5.32.1.1-32bit-portable.zip -oc:\Perl\
-    }
-    if ( -not (Test-Path 'C:\msys64' -PathType Container) ) { 
-        Write-Host "Installing msys..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-    }
-    if ( -not (Test-Path 'C:\boost_1_77_0' -PathType Container) ) { 
-        Write-Host "Installing Boost C++ Source Code..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        New-Item -Path "$env:HOMEDRIVE\$env:HOMEPATH\user-config.jam"
-        Add-Content -Path "$env:HOMEDRIVE\$env:HOMEPATH\user-config.jam" -Value 'using msvc : 14.1 ;'
-        Add-Content -Path "$env:HOMEDRIVE\$env:HOMEPATH\user-config.jam" -Value 'using gcc ;'
-        Add-Content -Path "$env:HOMEDRIVE\$env:HOMEPATH\user-config.jam" -Value 'using python : 3.7 : C:\\Program Files\\Python37 : C:\\Program Files\\Python37\\include : C:\\Program Files\\Python37\\libs ;'
-        7z x boost_1_77_0.zip -oc:\
-    }
-    if ( -not (Test-Path 'C:\openssl-master' -PathType Container) ) { 
-        Write-Host "Installing OpenSSL Source Code..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        7z x openssl.zip -oc:\
-        perl configure VC-WIN64A --prefix=C:\OpenSSL-Win64
-        nmake
-        nmake test
-        nmake install
-    }
-    if ( -not (Test-Path 'C:\gvsbuild-master' -PathType Container) ) { 
-        Write-Host "Installing GTK+3 Build Tools..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        7z x gvsbuild.zip -oc:\
-    }
-    if ( -not (Test-Path 'C:\libtorrent-RC_1_2' -PathType Container) ) { 
-        Write-Host "Installing Libtorrent Source Code..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        7z x libtorrent.zip -oc:\
-        Set-Location -Path 'C:\libtorrent-RC_1_2\bindings\python'
-        Invoke-BatchFile "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-        b2 libtorrent-link=static boost-link=static release toolset=msvc-14.1 optimization=space runtime-link=static link=static python=3.7 address-model=32 crypto=openssl
-    }
-    if ( -not (Test-Path 'C:\deluge-develop' -PathType Container) ) { 
-        Write-Host "Installing Deluge Source Code..."
-        Set-Location -Path 'C:\DelugeDownloads\'
-        7z x deluge.zip -oc:\
-    }
-    $env:Path = "C:\Program Files\7-Zip;C:\Program Files\Python37;C:\boost_1_77_0;C:\nasm-2.15;C:\Perl\bin;C:\msys64\bin;$env:Path"
-    $env:Path += ";C:\Program Files\7-Zip;C:\Program Files\Python37;C:\boost_1_77_0;C:\nasm-2.15;C:\Perl\bin;C:\msys64\bin;"
-    
-    Invoke-BatchFile "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-    Set-Location -Path 'C:\deluge-develop\'
-    python setup.py build
-    python setup.py install
 }
 
 if (test-is64bitOS) {
